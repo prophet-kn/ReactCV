4453d402e11a72d2f7f33dcae9d7bea040ef3aa0
Kacper Nowosielski <kacper.nowosielski@gmail.com>
2019-08-15 16:01:07 +0300

    even more

--- react-cv-kn/src/App.js
+++ react-cv-kn/src/App.js
@@ -60,20 +60,20 @@ function App() {
           <img src={pic} alt="Kacper Nowosielski"></img>
         </div>
         <div className={"cv-personal-information"}>
-          <h3>Personal Information</h3>
+          <h1>Personal Information</h1>
           <div className={"personal-information-content"}>
             <span>{personal.fullname}</span>
             <span>{personal.position}</span>
             <span>{personal.phone}</span>
             <span>{personal.email}</span>
             <div className={"icons"}>
-              <span><a href={personal.github}><svg width="30" height="30" viewBox="0 0 16 16" version="1.1"><path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg></a></span>
-              <span><a href={personal.linkedin}><svg width="30" height="30" viewBox="0 0 16 16"><g fillRule="nonzero"><path d="M3.733 13h-2.8V3.714h2.8V13zM14 13h-2.8V8.04c0-1.293-.463-1.936-1.38-1.936-.727 0-1.189.36-1.42 1.081V13H5.6s.037-8.357 0-9.286h2.21l.17 1.857h.059c.574-.928 1.491-1.558 2.75-1.558.956 0 1.73.265 2.32.93.595.666.891 1.559.891 2.813V13z"></path><ellipse cx="2.333" cy="1.393" rx="1.447" ry="1.393"></ellipse></g></svg></a></span>
+              <span><a href={personal.github}><svg width="40" height="40" viewBox="0 0 16 16" version="1.1"><path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg></a></span>
+              <span><a href={personal.linkedin}><svg width="40" height="40" viewBox="0 0 16 16"><g fillRule="nonzero"><path d="M3.733 13h-2.8V3.714h2.8V13zM14 13h-2.8V8.04c0-1.293-.463-1.936-1.38-1.936-.727 0-1.189.36-1.42 1.081V13H5.6s.037-8.357 0-9.286h2.21l.17 1.857h.059c.574-.928 1.491-1.558 2.75-1.558.956 0 1.73.265 2.32.93.595.666.891 1.559.891 2.813V13z"></path><ellipse cx="2.333" cy="1.393" rx="1.447" ry="1.393"></ellipse></g></svg></a></span>
             </div>
           </div>
         </div>
         <div className={"cv-languages"}>
-          <h3>Languages</h3>
+          <h1>Languages</h1>
           <div className={"languages-content"}>
             {lang.map((l, k) => {
               return (
@@ -83,15 +83,15 @@ function App() {
           </div>
         </div>
         <div className={"cv-experience"}>
-          <h3>Work Experience</h3>
+          <h1>Work Experience</h1>
           <div className={"experience-content cv-row"}>
             {_.orderBy(exp).reverse().map((e, k) => {
               if (e !== undefined) {
                 return (
                   <div key={k} className={"exp cv-col"}>
-                    <span>{e.time}</span>
-                    <span>{e.company}</span>
-                    <span>{e.title}</span>
+                    <span className={"label"}>{e.time}</span>
+                    <h3>{e.company}</h3>
+                    <h4>{e.title}</h4>
                     <span>{e.description}</span>
                   </div>
                 )
@@ -103,17 +103,17 @@ function App() {
           </div>
         </div>
         <div className={"cv-education"}>
-          <h3>Education</h3>
+          <h1>Education</h1>
           <div className={"education-content cv-row"}>
             {_.orderBy(edu).reverse().map((e, k) => {
               if (e !== undefined) {
                 return (
                   <div key={k} className={"edu cv-col"}>
-                    <span>{e.school}</span>
-                    <span>{e.time}</span>
+                    <span className={"label"}>{e.time}</span>
+                    <h3>{e.school}</h3>
                     <span>{e.programme}</span>
                     <span>{e.graduated}</span>
-                    {e.thesis !== undefined ? e.thesis : null}
+                    {e.thesis !== undefined ? <a href={e.thesis}>Thesis: Benefits of utilising Agile Scrum Management in Web Development projects : A case study at Mirum Agency</a> : null}
                   </div>
                 )
               }
@@ -124,12 +124,12 @@ function App() {
           </div>
         </div>
         <div className={"cv-techskills"}>
-          <h3>Tech Skills</h3>
+          <h1>Tech Skills</h1>
           <div className={"techskills-content"}>
             {tech.map((t, k) => {
               if (t !== undefined) {
                 return (
-                  <span key={k}>{t} </span>
+                  <span key={k}>{t}{tech.length !== k + 2 ? ', ' : ''}</span>
                 )
               }
               else {
@@ -139,12 +139,12 @@ function App() {
           </div>
         </div>
         <div className={"cv-otherskills"}>
-          <h3>Other Skills</h3>
+          <h1>Other Skills</h1>
           <div className={"otherskills-content"}>
             {other.map((o, k) => {
               if (o !== undefined) {
                 return (
-                  <span key={k}>{o} </span>
+                  <span key={k}>{o}{other.length !== k + 2 ? ', ' : ''}</span>
                 )
               }
               else {
@@ -154,12 +154,12 @@ function App() {
           </div>
         </div>
         <div className={"cv-interests"}>
-          <h3>Interests</h3>
+          <h1>Interests</h1>
           <div className={"interests-content"}>
             {int.map((i, k) => {
               if (i !== undefined) {
                 return (
-                  <span key={k}>{i} </span>
+                  <span key={k}>{i}{int.length !== k + 2 ? ', ' : ''}</span>
                 )
               }
               else {
@@ -169,12 +169,12 @@ function App() {
           </div>
         </div>
         <div className={"cv-notabletraits"}>
-          <h3>Notable Traits</h3>
-          <div className={"interests-content"}>
+          <h1>Notable Traits</h1>
+          <div className={"notabletraits-content"}>
             {traits.map((t, k) => {
               if (t !== undefined) {
                 return (
-                  <span key={k}>{t} </span>
+                  <span key={k}>{t}{traits.length !== k + 2 ? ', ' : ''}</span>
                 )
               }
               else {
