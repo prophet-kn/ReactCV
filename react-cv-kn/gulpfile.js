07891754d19e96610e9766824201399a66353505
Kacper Nowosielski <kacper.nowosielski@gmail.com>
2019-08-15 19:20:06 +0300

    finally works

--- react-cv-kn/gulpfile.js
+++ react-cv-kn/gulpfile.js
@@ -1,10 +1,9 @@
 'use strict'
 
-var gulp = require('gulp-param')(require('gulp'), process.argv);
+var gulp = require('gulp-param')(require('gulp'), process.argv)
 var sass = require('gulp-sass')
-var minifyInline = require('gulp-minify-inline');
-var bulkSass = require('gulp-sass-bulk-import');
-var sassLint = require('gulp-sass-lint');
+var bulkSass = require('gulp-sass-bulk-import')
+var sassLint = require('gulp-sass-lint')
 var cssDest = './src/'
 var scssSrc = './src/App.scss'
 var output = 'compressed'
@@ -13,19 +12,19 @@ sass.compiler = require('node-sass')
  
 gulp.task('sass', function () {
   return gulp.src(scssSrc)
-    .pipe(sassLint())
-    .pipe(minifyInline())
     .pipe(bulkSass())
+    .pipe(sassLint())
     .pipe(sass({
       outputStyle: output,
       precision: 10,
+      includePaths: ['./src'],
     }))
     .on('error', sass.logError)
     .pipe(gulp.dest(cssDest))
 })
  
-gulp.task('sass:watch', function () {
-  gulp.watch(scssSrc, gulp.series(['sass']))
+gulp.task('sass:watch', ['sass'], function() {
+  gulp.watch(scssSrc, ['sass'])
 })
 
-gulp.task('default', gulp.task(['sass:watch']))
+gulp.task('default', ['sass:watch'])
